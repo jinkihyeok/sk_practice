5d2622e4c7868a503a719460de1860dcfea1442a
jinki <jkhsky121@gmail.com>
2024-04-21 14:33:37 +0900

    Create ConsultationHistory UI

--- src/components/sections/ConsultationHistory.tsx
+++ src/components/sections/ConsultationHistory.tsx
@@ -1,10 +1,61 @@
-import React from "react";
-import SectionHeader from "../common/SectionHeader";
+import React, {useState} from "react";
+import {consultationHistoryTableHeaders, consultationHistoryTabs} from "../../constants";
 
 export default function ConsultationHistory() {
+    const [activeTab, setActiveTab] = useState(0);
+
     return (
-        <div className="section-style">
-        <SectionHeader title="고객상담이력" />
+        <div className="mt-4 text-xs">
+            <div className="flex justify-between">
+                <div className="flex flex-row items-center">
+                {consultationHistoryTabs.map((tab, index) => (
+                    <div
+                        key={index}
+                        className={`px-2 cursor-pointer border bg-gray-100 ${
+                            activeTab === index
+                                ? "font-semibold border-2"
+                                : "text-gray-700"
+                        }`}
+                        onClick={() => setActiveTab(index)}
+                    >
+                        {tab.title}
+                    </div>
+                ))}
+                </div>
+                <div className="flex flex-row gap-3 mb-2">
+                    <button className="btn-style px-3 !text-gray-400">번호관리 이력조회</button>
+                    <button className="btn-style px-3 !text-gray-400">고객접촉이력</button>
+                    <button className="btn-style px-3 !text-gray-400">고객정보현행화</button>
+                    <button className="btn-style px-3 !text-gray-400">SMS발송</button>
+                </div>
+            </div>
+            <table className="table-auto w-full">
+                <thead>
+                <tr className="table-header-style">
+                    {consultationHistoryTableHeaders[activeTab].map((header, index) => (
+                        <th key={index}>
+                            {header}
+                        </th>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {Array(5)
+                    .fill(null)
+                    .map((_, rowIndex) => (
+                        <tr
+                            key={rowIndex}
+                            className={rowIndex % 2 === 0 ? "bg-white" : "bg-gray-100"}
+                        >
+                            {consultationHistoryTableHeaders[activeTab].map((_, cellIndex) => (
+                                <td key={cellIndex} className="border text-center">
+                                    데이터 {rowIndex + 1}-{cellIndex + 1}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -10,3 +10,36 @@ export const serviceNumberOptions = [
 export const firstNumberOptions = [
     {value: '010', label: '010'}
 ];
+
+export const consultationHistoryTabs = [
+    { title: "고객상담이력" },
+    { title: "미납상담이력" },
+    { title: "SMS전송이력" },
+];
+
+export const consultationHistoryTableHeaders = [
+    [
+        "상담일자",
+        "상담시각",
+        "통화번호",
+        "상담유형",
+        "메모",
+        "처리상태",
+        "상담원",
+        "접촉구분",
+    ],
+    [
+        "상담일",
+        "상담시각",
+        "상담조치",
+        "통화유형",
+        "약속일",
+        "수납방법",
+        "약속금액",
+        "통화번호",
+        "상담원",
+        "센터",
+        "이벤트",
+    ],
+    ["발송일", "발송시각", "처리상태", "발송메시지", "회선번호", "발송자사번", "발송자명"],
+];
