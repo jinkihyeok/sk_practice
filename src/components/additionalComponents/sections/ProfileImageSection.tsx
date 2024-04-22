9aaf6ad9f431b6bae0e43ea242522fc03d32a2c0
jinki <jkhsky121@gmail.com>
2024-04-22 22:09:07 +0900

    max height 고정

--- src/components/additionalComponents/sections/ProfileImageSection.tsx
+++ src/components/additionalComponents/sections/ProfileImageSection.tsx
@@ -18,8 +18,8 @@ export default function ProfileImageSection() {
     return (
         <div className="col-span-1">
             <div className="flex flex-col w-full">
-                <div className="w-[250px] bg-red-200 relative border-2 border-gray-400">
-                    <img src={previewImage || "/images/profileImage.jpg"} alt="profile" className="w-[250px] object-cover"/>
+                <div className="w-[250px] max-h-[300.84px] relative border-2 border-gray-400">
+                    <img src={previewImage || "/images/profileImage.jpg"} alt="profile" className="w-[250px] max-h-[300.84px] object-cover"/>
                 </div>
                 <div className="flex justify-center w-[250px] my-2">
                     <label htmlFor="fileInput" className="flex flex-row justify-center items-center text-white bg-blue-500 w-full mx-1 py-2 border-2 border-gray-500 rounded-lg relative cursor-pointer">
