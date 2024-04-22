fc515ea4765046ce0576bedecdc7183c70fb2342
jinki <jkhsky121@gmail.com>
2024-04-22 11:29:22 +0900

    Create ConsultationDetailData.ts

---
+++ src/dummyData/ConsultationDetailData.ts
@@ -0,0 +1,26 @@
+export const ConsultationDetailData = [
+    {
+        "serviceNumber": "01046363519",
+        "corporateName": "",
+        "subscriptionDate": "2023-07-22",
+        "cancellationDate": "",
+        "serviceStatus": "사용 중",
+        "subscriptionType": "신규가입",
+        "manufacturer": "삼성",
+        "os": "Android 9",
+        "serialNumber": "353406",
+        "networkType": "LTE+WCDMA",
+        "numberPortability": "",
+        "welfareDicsount": "",
+        "planName": "10% 포인트(2GB/200분)",
+        "usageType": "일반",
+        "simType": "02",
+        "simName": "USIM",
+        "simFirstNumber": "12345",
+        "simSecondNumber": "123456789",
+        "deviceNickname": "OMD 삼성 갤럭시",
+        "deviceType": "스마트폰",
+        "isMinor": "N",
+        "legalGuardian": "",
+    }
+];
