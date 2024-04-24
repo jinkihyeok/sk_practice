a7893f9162c9df7b88fba37e291d3f1a4a9bda87
jinki <jkhsky121@gmail.com>
2024-04-24 18:13:21 +0900

    responsive

--- src/components/additionalComponents/sections/ImageSlideSection.tsx
+++ src/components/additionalComponents/sections/ImageSlideSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -15,46 +15,107 @@ const imageUrls = [
     "/images/slideImages/slideImage8.jpg",
     "/images/slideImages/slideImage9.jpg",
     "/images/slideImages/slideImage10.jpg",
-    ];
+];
 
-const PrevArrow: React.FC<{ onClick?: () => void }> = ({ onClick }) => (
+const PrevArrow: React.FC<{ onClick?: () => void }> = ({onClick}) => (
     <button
         className="absolute top-1/2 -left-10 transform -translate-y-1/2 z-10 text-black border-2 border-gray-500 rounded-full pr-2 py-2 bg-gray-300"
         onClick={onClick}
     >
-        <LeftArrowIcon />
+        <LeftArrowIcon/>
     </button>
 );
 
-const NextArrow: React.FC<{ onClick?: () => void }> = ({ onClick }) => (
+const NextArrow: React.FC<{ onClick?: () => void }> = ({onClick}) => (
     <button
         className="absolute top-1/2 -right-10 transform -translate-y-1/2 z-10 text-black border-2 border-gray-500 rounded-full pr-2 py-2 bg-gray-300"
         onClick={onClick}
     >
-        <RightArrowIcon />
+        <RightArrowIcon/>
     </button>
 );
 
 export default function ImageSlideSection() {
+
     const settings = {
+        className: "w-full max-w-[1740px]",
         dots: false,
         infinite: true,
         speed: 500,
         // autoplay: true,
         autoplaySpeed: 2000,
-        slidesToShow: 4,
+        slidesToShow: 6,
         slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        centerPadding: "0px"
+        nextArrow: <NextArrow/>,
+        prevArrow: <PrevArrow/>,
+        responsive: [
+            {
+                breakpoint: 1870,
+                settings: {
+                    className: "w-full max-w-[1640px]",
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 1770,
+                settings: {
+                    className: "w-full max-w-[1540px]",
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 1670,
+                settings: {
+                    className: "w-full max-w-[1440px]",
+                    slidesToShow: 5,
+                },
+            },
+            {
+                breakpoint: 1570,
+                settings: {
+                    className: "w-full max-w-[1340px]",
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 1470,
+                settings: {
+                    className: "w-full max-w-[1240px]",
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 1370,
+                settings: {
+                    className: "w-full max-w-[1140px]",
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1270,
+                settings: {
+                    className: "w-full max-w-[1040px]",
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1170,
+                settings: {
+                    className: "w-full max-w-[880px]",
+                    slidesToShow: 3,
+                },
+            }
+        ]
     }
+
 
     return (
         <div className="flex items-center w-full h-56 border-2 border-gray-500 justify-center">
-            <Slider {...settings} className="w-full max-w-[1100px]">
+            <Slider {...settings}>
                 {imageUrls.map((url, index) => (
                     <div key={index} className="px-5">
-                        <img src={url} alt={`slideImage${index}`} className="w-[250px] h-[180px] object-cover border-2 border-gray-500"/>
+                        <img src={url} alt={`slideImage${index}`}
+                             className="w-[250px] h-[180px] object-cover border-2 border-gray-500"/>
                     </div>
                 ))}
             </Slider>
